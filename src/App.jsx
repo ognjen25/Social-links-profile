b69666e004c6cc381415033f3acd0987655b0742
ognjen25 <ognjendabic640@gmail.com>
2026-03-14 10:51:06 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+function App() {
+
+  return (
+    <section class="bg-grey-800 min-h-144.75 max-w-72 m-auto p-spacing-300 flex gap-spacing-300 flex-col place-items-center">
+      
+      <img class="max-w-22 rounded-full" src="/src/assets/images/avatar-jessica.jpeg" alt="avatar_pic"
+        loading="lazy" />
+      
+      <div class="text-center w-full">
+        <h1 class="text_preset_1">Jessica Randall</h1>
+        <h3 class="text_preset_2_bold text-green">London, United Kingdom</h3>
+        <p class="text_preset_2 mt-spacing-300">"Front-end developer and avid reader."</p>
+      </div>
+      
+      <ul>
+        <li>
+          <p>GitHub</p>
+        </li>
+        <li>
+          <p>Frontend Mentor</p>
+        </li>
+        <li>
+          <p>LinkedIn</p>
+        </li>
+        <li>
+          <p>Twitter</p>
+        </li>
+        <li>
+          <p>Instagram</p>
+        </li>
+      </ul>
+    </section>
+  )
+}
+
+export default App
---
+++ src/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return (
+        <div class="w-full text-[11px] text-center">
+            Challenge by <a class="text-green"
+                href="https://www.frontendmentor.io?ref=challenge"
+                target="_blank">Frontend Mentor</a>.
+            Coded by <a class="text-green" href="#">Your Name Here</a>.
+        </div>
+    )
+}
+
+export default Footer
