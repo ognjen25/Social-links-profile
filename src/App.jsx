673f95fd7df7930d1c59fab2f68b635d579c6bf5
ognjen25 <ognjendabic640@gmail.com>
2026-03-20 19:08:37 +0100

    image fix and basic layout finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,34 @@
+import avatar from './assets/images/avatar-jessica.jpeg';
+
 function App() {
 
   return (
-    <section class="bg-grey-800 min-h-144.75 max-w-72 m-auto p-spacing-300 flex gap-spacing-300 flex-col place-items-center">
+    <section className="bg-grey-800 min-h-144.75 max-w-72 m-500 rounded-xl p-300 flex gap-300 flex-col justify-baseline place-items-center">
       
-      <img class="max-w-22 rounded-full" src="./src/assets/images/avatar-jessica.jpeg" alt="avatar_pic"
+      <img className="max-w-22 rounded-full" src={avatar} alt="avatar_pic"
         loading="lazy" />
       
-      <div class="text-center w-full">
-        <h1 class="text_preset_1">Jessica Randall</h1>
-        <h3 class="text_preset_2_bold text-green">London, United Kingdom</h3>
-        <p class="text_preset_2 mt-spacing-300">"Front-end developer and avid reader."</p>
+      <div className="text-center w-full">
+        <h1 className="text_preset_1">Jessica Randall</h1>
+        <h3 className="text_preset_2_bold text-green">London, United Kingdom</h3>
+        <p className="text_preset_2">"Front-end developer and avid reader."</p>
       </div>
       
-      <ul>
-        <li>
-          <p>GitHub</p>
+      <ul className="text_preset_2_bold text-center w-full ">
+        <li className="list_elements">
+          <p className="block w-full">GitHub</p>
         </li>
-        <li>
-          <p>Frontend Mentor</p>
+        <li className="list_elements">
+          <p className="block w-full">Frontend Mentor</p>
         </li>
-        <li>
-          <p>LinkedIn</p>
+        <li className="list_elements">
+          <p className="block w-full">LinkedIn</p>
         </li>
-        <li>
-          <p>Twitter</p>
+        <li className="list_elements">
+          <p className="block w-full">Twitter</p>
         </li>
-        <li>
-          <p>Instagram</p>
+        <li className="list_elements">
+          <p className="block w-full">Instagram</p>
         </li>
       </ul>
     </section>
